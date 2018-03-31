f6625c93245ecf2385b6083446e2bb2ada6f229e
Your Name <you@example.com>
2018-03-31 18:42:12 +0600

    init

--- core/src/com/kangaroo/ru/TurretPunch.java
+++ core/src/com/kangaroo/ru/TurretPunch.java
@@ -2,6 +2,7 @@ package com.kangaroo.ru;
 
 import com.badlogic.gdx.graphics.Texture;
 
+import java.awt.Rectangle;
 
 
 public class TurretPunch {
@@ -9,16 +10,19 @@ public class TurretPunch {
 
     private Texture colorTexture;
     private Texture frameTexture;
+    Rectangle punch;
 
     int height, width;
 
 
-    void texture() {
+    public void texture() {
         colorTexture = new Texture("turret_2_color.png");
-
-
-
         frameTexture = new Texture("turret_2_frame.png");
 
+        punch = new Rectangle();
+
+        punch.height = 231;
+        punch.width = 78;
     }
+
 }
