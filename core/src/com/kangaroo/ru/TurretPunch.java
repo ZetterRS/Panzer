eafcee1373ab87a66c92bba2aaa4d6cc7375847a
Your Name <you@example.com>
2018-03-31 18:33:37 +0600

    init

--- core/src/com/kangaroo/ru/TurretPunch.java
+++ core/src/com/kangaroo/ru/TurretPunch.java
@@ -2,9 +2,7 @@ package com.kangaroo.ru;
 
 import com.badlogic.gdx.graphics.Texture;
 
-/**
- * Created by Lenovo on 31.03.2018.
- */
+
 
 public class TurretPunch {
 
@@ -12,8 +10,15 @@ public class TurretPunch {
     private Texture colorTexture;
     private Texture frameTexture;
 
+    int height, width;
+
+
     void texture() {
         colorTexture = new Texture("turret_2_color.png");
+
+
+
         frameTexture = new Texture("turret_2_frame.png");
+
     }
 }
