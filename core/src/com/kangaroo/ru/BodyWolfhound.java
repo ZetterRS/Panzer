3328dcd5526534006f5630929ef266730ce1426c
Your Name <you@example.com>
2018-04-01 10:06:08 +0600

    init

--- core/src/com/kangaroo/ru/BodyWolfhound.java
+++ core/src/com/kangaroo/ru/BodyWolfhound.java
@@ -2,14 +2,22 @@ package com.kangaroo.ru;
 
 import com.badlogic.gdx.graphics.Texture;
 
+import java.awt.Rectangle;
+
 
 public class BodyWolfhound {
 
     private Texture colorTexture;
     private Texture frameTexture;
+    Rectangle wolfhound;
+
+    int width, height;
 
     void texture(){
         colorTexture = new Texture("body_1_color.png");
         frameTexture = new Texture ("body_1_frame.png");
+
+        wolfhound.height = 231;
+        wolfhound.width = 122;
     }
 }
--- core/src/com/kangaroo/ru/TurretCrusher.java
+++ core/src/com/kangaroo/ru/TurretCrusher.java
@@ -3,13 +3,21 @@ package com.kangaroo.ru;
 
 import com.badlogic.gdx.graphics.Texture;
 
+import java.awt.Rectangle;
+
 public class TurretCrusher {
 
     private Texture colorTexture;
     private Texture frameTexture;
+    Rectangle crusher;
+
+    int height, width;
 
     void texture () {
         colorTexture = new Texture("m54_color.png");
         frameTexture = new Texture("m54_frame.png");
+
+        crusher.height = 231;
+        crusher.width = 78;
     }
 }
